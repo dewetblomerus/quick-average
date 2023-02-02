9b575a6009cae65b86652dc0ec2783aaaa79773e
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-02-02 06:15:07 -0500

    New LiveView page ✨

---
+++ lib/quick_average_web/live/average_live.ex
@@ -0,0 +1,9 @@
+defmodule QuickAverageWeb.AverageLive do
+  use QuickAverageWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    Average Hello
+    """
+  end
+end
--- lib/quick_average_web/router.ex
+++ lib/quick_average_web/router.ex
@@ -2,22 +2,23 @@ defmodule QuickAverageWeb.Router do
   use QuickAverageWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {QuickAverageWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, {QuickAverageWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", QuickAverageWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
+    live("/:room_id", AverageLive)
   end
 
   # Other scopes may use custom stacks.
@@ -35,10 +36,10 @@ defmodule QuickAverageWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: QuickAverageWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: QuickAverageWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
