d61338fd9ae7d5e005347e1a65819b04fc79af9a
De Wet Blomerus <dewetblomerus@gmail.com>
2025-10-17 17:15:25 -0400

    Upgrade everything else

--- lib/quick_average_web/components/core_components.ex
+++ lib/quick_average_web/components/core_components.ex
@@ -92,17 +92,17 @@ defmodule QuickAverageWeb.CoreComponents do
                     id={"#{@id}-title"}
                     class="text-lg font-semibold leading-8 text-zinc-800"
                   >
-                    <%= render_slot(@title) %>
+                    {render_slot(@title)}
                   </h1>
                   <p
                     :if={@subtitle != []}
                     id={"#{@id}-description"}
                     class="mt-2 text-sm leading-6 text-zinc-600"
                   >
-                    <%= render_slot(@subtitle) %>
+                    {render_slot(@subtitle)}
                   </p>
                 </header>
-                <%= render_slot(@inner_block) %>
+                {render_slot(@inner_block)}
                 <div
                   :if={@confirm != [] or @cancel != []}
                   class="ml-6 mb-4 flex items-center gap-5"
@@ -114,14 +114,14 @@ defmodule QuickAverageWeb.CoreComponents do
                     phx-disable-with
                     class="py-2 px-3"
                   >
-                    <%= render_slot(confirm) %>
+                    {render_slot(confirm)}
                   </.button>
                   <.link
                     :for={cancel <- @cancel}
                     phx-click={hide_modal(@on_cancel, @id)}
                     class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
                   >
-                    <%= render_slot(cancel) %>
+                    {render_slot(cancel)}
                   </.link>
                 </div>
               </div>
@@ -184,9 +184,9 @@ defmodule QuickAverageWeb.CoreComponents do
       >
         <Heroicons.cake :if={@kind == :info} mini class="h-4 w-4" />
         <Heroicons.exclamation_circle :if={@kind == :error} mini class="h-4 w-4" />
-        <%= @title %>
+        {@title}
       </p>
-      <p class="mt-2 text-[0.8125rem] leading-5"><%= msg %></p>
+      <p class="mt-2 text-[0.8125rem] leading-5">{msg}</p>
       <button
         :if={@close}
         type="button"
@@ -232,12 +232,12 @@ defmodule QuickAverageWeb.CoreComponents do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
       <div class="space-y-4 bg-white mb-5">
-        <%= render_slot(@inner_block, f) %>
+        {render_slot(@inner_block, f)}
         <div
           :for={action <- @actions}
           class="mt-2 flex items-center justify-between gap-6"
         >
-          <%= render_slot(action, f) %>
+          {render_slot(action, f)}
         </div>
       </div>
     </.form>
@@ -269,7 +269,7 @@ defmodule QuickAverageWeb.CoreComponents do
       ]}
       {@rest}
     >
-      <%= render_slot(@inner_block) %>
+      {render_slot(@inner_block)}
     </button>
     """
   end
@@ -353,7 +353,7 @@ defmodule QuickAverageWeb.CoreComponents do
         class="rounded border-zinc-300 text-zinc-900 focus:ring-zinc-900"
         {@rest}
       />
-      <%= @label %>
+      {@label}
     </label>
     """
   end
@@ -377,7 +377,7 @@ defmodule QuickAverageWeb.CoreComponents do
       />
       <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
       </div>
-      <%= @label %>
+      {@label}
     </label>
     """
   end
@@ -385,7 +385,7 @@ defmodule QuickAverageWeb.CoreComponents do
   def input(%{type: "select"} = assigns) do
     ~H"""
     <div phx-feedback-for={@name}>
-      <.label for={@id}><%= @label %></.label>
+      <.label for={@id}>{@label}</.label>
       <select
         id={@id}
         name={@name}
@@ -393,10 +393,10 @@ defmodule QuickAverageWeb.CoreComponents do
         multiple={@multiple}
         {@rest}
       >
-        <option :if={@prompt} value=""><%= @prompt %></option>
-        <%= Phoenix.HTML.Form.options_for_select(@options, @value) %>
+        <option :if={@prompt} value="">{@prompt}</option>
+        {Phoenix.HTML.Form.options_for_select(@options, @value)}
       </select>
-      <.error :for={msg <- @errors}><%= msg %></.error>
+      <.error :for={msg <- @errors}>{msg}</.error>
     </div>
     """
   end
@@ -404,7 +404,7 @@ defmodule QuickAverageWeb.CoreComponents do
   def input(%{type: "textarea"} = assigns) do
     ~H"""
     <div phx-feedback-for={@name}>
-      <.label for={@id}><%= @label %></.label>
+      <.label for={@id}>{@label}</.label>
       <textarea
         id={@id || @name}
         name={@name}
@@ -417,7 +417,7 @@ defmodule QuickAverageWeb.CoreComponents do
         {@rest}
       >
     <%= @value %></textarea>
-      <.error :for={msg <- @errors}><%= msg %></.error>
+      <.error :for={msg <- @errors}>{msg}</.error>
     </div>
     """
   end
@@ -425,7 +425,7 @@ defmodule QuickAverageWeb.CoreComponents do
   def input(assigns) do
     ~H"""
     <div phx-feedback-for={@name}>
-      <.label for={@id}><%= @label %></.label>
+      <.label for={@id}>{@label}</.label>
       <input
         type={@type}
         name={@name}
@@ -439,7 +439,7 @@ defmodule QuickAverageWeb.CoreComponents do
         ]}
         {@rest}
       />
-      <.error :for={msg <- @errors}><%= msg %></.error>
+      <.error :for={msg <- @errors}>{msg}</.error>
     </div>
     """
   end
@@ -459,7 +459,7 @@ defmodule QuickAverageWeb.CoreComponents do
   def label(assigns) do
     ~H"""
     <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
-      <%= render_slot(@inner_block) %>
+      {render_slot(@inner_block)}
     </label>
     """
   end
@@ -476,7 +476,7 @@ defmodule QuickAverageWeb.CoreComponents do
         mini
         class="mt-0.5 h-5 w-5 flex-none fill-rose-500"
       />
-      <%= render_slot(@inner_block) %>
+      {render_slot(@inner_block)}
     </p>
     """
   end
@@ -498,13 +498,13 @@ defmodule QuickAverageWeb.CoreComponents do
     ]}>
       <div>
         <h1 class="text-lg font-semibold leading-8 text-zinc-800">
-          <%= render_slot(@inner_block) %>
+          {render_slot(@inner_block)}
         </h1>
         <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
-          <%= render_slot(@subtitle) %>
+          {render_slot(@subtitle)}
         </p>
       </div>
-      <div class="flex-none"><%= render_slot(@actions) %></div>
+      <div class="flex-none">{render_slot(@actions)}</div>
     </header>
     """
   end
@@ -537,10 +537,10 @@ defmodule QuickAverageWeb.CoreComponents do
         <thead class="text-left text-[0.8125rem] leading-6 text-zinc-500">
           <tr>
             <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal">
-              <%= col[:label] %>
+              {col[:label]}
             </th>
             <th class="relative p-0 pb-4">
-              <span class="sr-only"><%= gettext("Actions") %></span>
+              <span class="sr-only">{gettext("Actions")}</span>
             </th>
           </tr>
         </thead>
@@ -561,7 +561,7 @@ defmodule QuickAverageWeb.CoreComponents do
               </div>
               <div class="block py-4 pr-6">
                 <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
-                  <%= render_slot(col, row) %>
+                  {render_slot(col, row)}
                 </span>
               </div>
             </td>
@@ -571,7 +571,7 @@ defmodule QuickAverageWeb.CoreComponents do
                   :for={action <- @action}
                   class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
                 >
-                  <%= render_slot(action, row) %>
+                  {render_slot(action, row)}
                 </span>
               </div>
             </td>
@@ -602,9 +602,9 @@ defmodule QuickAverageWeb.CoreComponents do
       <dl class="-my-4 divide-y divide-zinc-100">
         <div :for={item <- @item} class="flex gap-4 py-4 sm:gap-8">
           <dt class="w-1/4 flex-none text-[0.8125rem] leading-6 text-zinc-500">
-            <%= item.title %>
+            {item.title}
           </dt>
-          <dd class="text-sm leading-6 text-zinc-700"><%= render_slot(item) %></dd>
+          <dd class="text-sm leading-6 text-zinc-700">{render_slot(item)}</dd>
         </div>
       </dl>
     </div>
@@ -629,7 +629,7 @@ defmodule QuickAverageWeb.CoreComponents do
         class="text-sm font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
       >
         <Heroicons.arrow_left solid class="w-3 h-3 stroke-current inline" />
-        <%= render_slot(@inner_block) %>
+        {render_slot(@inner_block)}
       </.link>
     </div>
     """
