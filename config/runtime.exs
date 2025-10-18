4e2d6fe7765fdc62e6e00b6be5e369bc87525568
De Wet Blomerus <dewetblomerus@gmail.com>
2025-10-18 11:56:47 -0400

    Clustering working 🥳

--- config/runtime.exs
+++ config/runtime.exs
@@ -37,6 +37,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  config :quick_average, :dns_cluster_query, System.fetch_env!("DNS_CLUSTER_QUERY")
+
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
